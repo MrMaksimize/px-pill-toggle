ec087bbdaebbcdb9b1a5d4b5391c7988d74e22a7
Maksim Pecherskiy <max@maksimize.com>
2017-08-21 16:48:38 -0700

    Initial toggle switch.  This is a mess and still needs a ton of cleanup.

--- px-toggle.es6.js
+++ px-toggle.es6.js
@@ -7,17 +7,7 @@
       /* Properties for this component */
       properties: {
 
-          /**
-           * The size of the toggle element. Can be one of
-           * small, regular (default), large & huge.
-           * @type {String}
-           */
-          size: {
-            type: String,
-            value: ""
-          },
-
-          /**
+         /**
            * The disabled state attribute for the component.
            * @type {Boolean}
            * @default false
@@ -26,14 +16,22 @@
             type: Boolean,
             value: false,
             reflectToAttribute: true
+          },
+          items: {
+            type: Array,
+            reflectToAttribute: true
+          },
+          selected: {
+            type: String,
+            reflectToAttribute: true,
+            notify: true
           }
-      },
-      listeners : {
-        'tap': '_onCheckTap'
       },
 
       attached() {
         this.setAttribute('role','switch');
+        if (this.selected)
+          Polymer.dom(this.root).querySelector('li#' + this.selected).classList.add("active");
       },
 
       _checkDisabledState(disabled){
@@ -41,20 +39,21 @@
         return `${(disabled ? 'toggle--disabled' : '')}`;
       },
 
-      _checkLabelSize(size){
-        return `toggle__label--${size}`;
-      },
 
-      _checkInputSize(size){
-        return `toggle__input--${size}`;
-      },
-
-      _onCheckTap(evt) {
+      onChangeSelect(evt) {
+        console.log(evt);
+        const el = evt.srcElement;
+        const parentEl = evt.srcElement.parentElement;
         if(!this.disabled) {
-          this.debounce('checkChanged', function() {
-            this.checked = !this.checked;
+          this.debounce('checkSelect', function() {
+            this.set('selected', evt.srcElement.id);
+            Polymer.dom(this.root).querySelectorAll('li').forEach((el) => {
+                el.classList.remove("active");
+            });
+            parentEl.classList.add("active");
           },50);
         }
+        return false;
       }
     });
   }
